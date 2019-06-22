b3e695ad5be9663d61790c6131e86de12d70c0a6
DoXuanDung <doxuandungx10@gmail.com>
2019-06-22 16:58:39 +0700

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,30 @@
+console.log("Hello Fuch, i'm so happy to see you")
+
+// let x = 3 // có thể thay đổi giá trị của biến
+// var y = 5 // có thể thay đổi giá trị của biến
+// const z = 6 // khoong thể thay đổi giá trị của biến
+
+// x = 5
+// y = 7
+
+// console.log(x,y,z)
+
+// number: 3 4 5 3.345, 789
+// string: hello how are you, 09a0aew98ra0
+// var x , y ;
+// x = prompt("ban hay nhap x")
+
+// y = prompt("ban hay nhap y")
+
+// console.log((x + y))
+// // console.log("Hello")
+// // console.log("sklfksjfglk")
+// let x = Number(prompt("enter x="))
+// let y = Number(prompt("enter y="))
+// console.log(x - y)
+
+// for (đk lặp){}
+
+for (let i = 0; i < 4; i--){
+    console.log("hihi")
+}
